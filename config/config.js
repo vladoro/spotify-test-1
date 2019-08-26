d591747dadc6fdb01144d32558c6c33ba65617aa
Kyle Tranel <k_tranel@yahoo.com>
2019-08-26 11:09:20 -0500

    initial commit

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const username = process.env.NAME;
+const password = process.env.PASSWORD;
+const database = process.env.DATABASE;
+const host = process.env.HOST;
+const node_env = process.env.NODE_ENV;
+const session_secret = process.env.SESSION_SECRET;
+
+const config = {
+    dev:{
+        node_env: node_env,
+        db:{
+            username,
+            password,
+            database,
+            host
+        },
+        session_secret
+    },
+    staging:{},
+    prod:{}
+};
+
+module.exports = config[node_env];
